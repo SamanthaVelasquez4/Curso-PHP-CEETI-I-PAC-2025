383dbd04d3482c607375b653429d2047a7fd84b9
SamanthaVelasquez4 <samvel200355@gmail.com>
2025-04-01 12:49:39 -0600

    Clase 2

---
+++ Semana 1/Clase 2/index0.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo</title>
+</head>
+<body>
+    <h1>Hola Mundo con HTML!</h1>
+    <?php
+        echo "<h2>Este es una linea de php</h2>";
+    ?>
+</body>
+</html>
---
+++ Semana 1/Clase 2/prueba.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Esta linea va a ser ejecutada en consola";
